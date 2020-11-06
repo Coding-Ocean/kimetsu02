a21548bbe0a8cbd5a1c7485171088242673e1c74
Coding-Ocean <eijo524@gmail.com>
2020-11-06 11:26:07 +0900

    complete

--- gmain.cpp
+++ gmain.cpp
@@ -4,28 +4,29 @@ void gmain() {
     window(1920, 1080, full);
     //変数定義
     int img[2] = { 0,0 }, idx = 0;
-    float x = 0, y = 0, dx = 0, dy = 0;
+    float x = 0, y = 0, dx = 0, dy = 0, ofsX = 0;
     int cols = 0, rows = 0;
     //初期値設定
     img[0] = loadImage("assets/cube0.png");
     img[1] = loadImage("assets/cube1.png");
     dx = 140;//横にずらす値
     dy = 120;//縦にずらす値
-    cols = width / dx + 1;//列数
-    rows = height / dy + 1;//行数
+    cols = int(width / dx) + 2;//列数
+    rows = int(height / dy) + 1;//行数
     //１回だけ描画
     rectMode(CENTER);
     for (int j = 0; j < rows; j++) {
         y = dy * j;
         for (int i = 0; i < cols; i++) {
-            //奇数行はdx/2ずらす
-            x = (j % 2) * (dx / 2) + dx * i;
+            //奇数行は(dx/2)分ずらす
+            ofsX = (j % 2) * (dx / 2);
+            x = ofsX + dx * i;
             idx = (j / 2 + i) % 2;
             image(img[idx], x, y);
         }
     }
     //アニメーションさせないので
-    //描画後、からループさせているだけ
+    //描画後、空ループさせているだけ
     while (notQuit) {
     }
 }
